469e9a724898794a82ada6bb54638765b72ef8d9
timothy-r <tim.rodger@gmail.com>
2022-06-18 10:39:29 +0100

    Adds indexfilter function

--- functions/imagePage.js
+++ functions/imagePage.js
@@ -1,7 +1,5 @@
 'use strict';
 
-const AWS = require('aws-sdk');
-//var sns = new AWS.SNS({apiVersion: '2010-03-31'});
 const snsWrapper = require('lib/snsWrapper');
 
 
---
+++ functions/indexFilter.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({apiVersion: '2006-03-01'});
+const snsWrapper = require('lib/snsWrapper');
+
+/**
+ * for each new page generated, ensure an idex page exists within each directory
+ * directories & sub-dirs are created by adding / chars in object paths
+ * strategy - 
+ * a router lambda function (this one) to split all object paths into the dirs that need indexes
+ * another lambda function that generates an index page for a single directory
+ * 
+ *
+ * @param event
+ * @param context
+ * @param callback
+ */
+module.exports.handler = (event, context, callback) => {
+
+    const message = snsWrapper.getSnsMessage(event);
+
+    console.log(JSON.stringify(message));
+
+    const directories = message.pathName.split('/')
+
+    // publish multiple messages
+
+    snsWrapper.publish(
+        'directory.created',
+        {
+            event: event,
+            dirName: dir
+        },
+        process.env.DIRECTORY_PAGE_TOPIC,
+        callback
+    );
+};
